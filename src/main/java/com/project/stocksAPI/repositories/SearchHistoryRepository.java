96fda3d065d8472ff5a6dd381dac85f6cd389100
pedro <pedromatias0047@gmail.com>
2025-10-10 01:49:27 -0300

    Tweak: rearranged files

--- src/main/java/com/project/stocksAPI/repositories/SearchHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.stocksAPI.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.stocksAPI.models.SearchHistory;
-
-public interface SearchHistoryRepository extends JpaRepository<SearchHistory, Long> {
-	// Ao acordar, Gregor Samsa havia virado um inseto. E sua primeira preocupação… era não se atrasar pro trabalho.
-}
